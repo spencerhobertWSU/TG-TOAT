4c44f5faa80aad35d1c40c2b60657f02ced20f23
Joshua Morgan <54748973+joshuamorganWSU@users.noreply.github.com>
2024-09-16 16:11:03 -0600

    Revert "finish Calendar task"
    This reverts commit 6087f0af7ce98f09266d75ff03958b87d02d6e12.

--- TG-TOAT/Controllers/UserController.cs
+++ TG-TOAT/Controllers/UserController.cs
@@ -30,12 +30,6 @@ namespace TGTOAT.Controllers
             return View();
         }
 
-        // Calendar action
-        public IActionResult Calendar()
-        {
-            return View(); // This will look for Views/User/Calendar.cshtml
-        }
-
         public async Task<IActionResult> Login(string Email, string Password)
         {
             // Grab the user by email

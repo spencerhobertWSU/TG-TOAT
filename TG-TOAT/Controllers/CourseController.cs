3b042c201d84f522145786196658c7c97ad06aee
Joshua Morgan <54748973+joshuamorganWSU@users.noreply.github.com>
2024-09-27 00:52:25 -0600

    updated the view model for the courses view but can't seem to get the calendar to populate with events from the database.

--- TG-TOAT/Controllers/CourseController.cs
+++ TG-TOAT/Controllers/CourseController.cs
@@ -7,16 +7,20 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using System.Drawing.Printing;
+using TGTOAT.Helpers;
+using TGTOAT.ViewModels;
 
 namespace TGTOAT.Controllers
 {
     public class CourseController : Controller
     {
         private readonly UserContext _context;
+        private readonly IAuthentication _authentication;
 
-        public CourseController(UserContext context)
+        public CourseController(UserContext context, IAuthentication authentication)
         {
             _context = context;
+            _authentication = authentication;
         }
 
         // Display the AddCourse view with the list of departments
@@ -90,14 +94,22 @@ namespace TGTOAT.Controllers
             };
             model.Instructors = _context.User.Where(u => u.UserRole == "Instructor").ToList();
             return View(model);
+        }       
+
+        public IActionResult Courses()
+        {            
+            var courses = _context.Courses.Include(c => c.Department).ToList();            
+            var currentUser = _authentication.CheckUser();
+
+            // Create the ViewModel that combines courses and user info
+            var viewModel = new CourseListViewModel
+            {
+                Courses = courses,
+                UserLoginViewModel = currentUser
+            };           
+            return View(viewModel);  
         }
 
-        // Placeholder for course listing page
-        public IActionResult Courses()
-        {
-            var courses = _context.Courses.Include(c => c.Department).ToList();
-            return View(courses); // This will look for Views/Course/Courses.cshtml
-        }
         public IActionResult ViewCourse(int id)
         {
             var course = _context.Courses.Include(c => c.Department).FirstOrDefault(c => c.CourseId == id);
@@ -107,5 +119,73 @@ namespace TGTOAT.Controllers
             }
             return View(course);
         }
+
+        // get courses for calendar view
+        [HttpGet]
+        public JsonResult GetCoursesForCalendar()
+        {
+            string currentInstructorId = _authentication.GetCurrentUserId();
+
+            if (string.IsNullOrEmpty(currentInstructorId))
+            {
+                return Json(new { error = "No instructor logged in." });
+            }
+
+            var courses = _context.InstructorCourseConnection
+                .Include(icc => icc.Course)
+                .Where(icc => icc.InstructorID == int.Parse(currentInstructorId))
+                .Select(icc => icc.Course)
+                .ToList();
+
+            var courseEvents = new List<object>();
+            var dayMap = new Dictionary<char, int> {
+                { 'M', 1 },
+                { 'T', 2 },
+                { 'W', 3 },
+                { 'R', 4 },  // Thursday
+                { 'F', 5 }
+            };
+
+            foreach (var course in courses)
+            {
+                if (course.StartTime.HasValue && course.EndTime.HasValue)
+                {
+                    var startTime = course.StartTime.Value;
+                    var endTime = course.EndTime.Value;
+
+                    // Convert DaysOfTheWeek to uppercase to match dayMap keys
+                    foreach (char day in course.DaysOfTheWeek.ToUpper())
+                    {
+                        if (dayMap.ContainsKey(day))
+                        {
+                            int dayCode = dayMap[day];
+                            courseEvents.Add(new
+                            {
+                                title = course.CourseName,
+                                start = new DateTime(2024, 9, 1, startTime.Hour, startTime.Minute, startTime.Second).ToString("yyyy-MM-ddTHH:mm:ss"),
+                                end = new DateTime(2024, 9, 1, endTime.Hour, endTime.Minute, endTime.Second).ToString("yyyy-MM-ddTHH:mm:ss"),
+                                daysOfWeek = new[] { dayCode },
+                                startRecur = "2024-09-01",
+                                endRecur = "2024-12-15"
+                            });
+                        }
+                    }
+                }
+            }
+
+            // Test event 
+            courseEvents.Add(new
+            {
+                title = "Test Course",
+                start = "2024-09-01T09:00:00",
+                end = "2024-09-01T10:00:00",
+                daysOfWeek = new[] { 1, 3, 5 },  // Monday, Wednesday, Friday
+                startRecur = "2024-09-01",
+                endRecur = "2024-12-15"
+            });
+
+            return Json(courseEvents);
+        }
+
     }
 }
---
+++ TG-TOAT/Models/CourseListViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace TGTOAT.ViewModels
+{
+    public class CourseListViewModel
+    {
+        public IEnumerable<TGTOAT.Data.Courses> Courses { get; set; }
+        public TGTOAT.Models.UserLoginViewModel UserLoginViewModel { get; set; }
+    }
+}

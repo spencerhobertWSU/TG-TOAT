52738b801b18a58d8e2cb3a0bedf8bd029c65791
Spencer Hobert <spencerhobert@mail.weber.edu>
2024-11-14 20:20:09 -0700

    FINISHED

--- TGTOATTest/Spencer_Unit_Tests.cs
+++ TGTOATTest/Spencer_Unit_Tests.cs
@@ -32,7 +32,14 @@ namespace Spencer_Unit_Tests
             driver = new ChromeDriver();
 
             // Navigate to the website
-            driver.Navigate().GoToUrl("https://localhost:44362/");
+            try
+            {
+                driver.Navigate().GoToUrl("https://localhost:44362/");
+            }
+            catch
+            {
+                driver.Navigate().GoToUrl("https://localhost:7244/");
+            }
 
             // Login to the website (IDK WHY BUT YOU HAVE TO SEND THE ENTER KEY? YOU CAN'T CLICK IT?)
             driver.FindElement(By.Id("Email")).SendKeys("bob@gmail.com");
@@ -154,66 +161,58 @@ namespace Spencer_Unit_Tests
                 InstructorId = instructor.UserId,
                 CourseId = course.CourseId
             };
-            StudentCourseConnection studentCourseConnection = new StudentCourseConnection
+            StudentConnection studentCourseConnection = new StudentConnection
             {
-                StudentCourseId = 1,
-                StudentID = student.Id,
-                User = student,
-                CourseId = course.CourseId,
-                Course = course
+                StudentId = student.UserId,
+                CourseId = course.CourseId
             };
-            course.instructorCourseConnections = new List<InstructorCourseConnection> { instructorCourseConnection };
-            course.StudentCourseConnection = new List<StudentCourseConnection> { studentCourseConnection };
 
             // Create the quiz
             Assignment quiz = new Assignment
             {
-                AssignmentId = 1,
-                InstructorCourseId = instructorCourseConnection.InstructorCourseConnectionId,
-                InstructorCourse = instructorCourseConnection,
-                AssignmentDescription = "Test Quiz",
-                AssignmentName = "Quiz",
-                DueDateAndTime = new System.DateTime(2021, 12, 31),
-                AssignmentPoints = 100,
-                AssignmentType = "Text",
+                AssignId = 1,
                 CourseId = course.CourseId,
-                Course = course
+                AssignName = "Quiz",
+                AssignDesc = "Test Quiz",
+                AssignType = "Text",
+                MaxPoints = 100,
+                DueDate = DateTime.Now,
             };
 
             // Connect the student to the quiz
-            StudentAssignments studentAssignment = new StudentAssignments
+            StudentAssignments studentQuiz = new StudentAssignments
             {
-                AssignmentGradeId = 1,
-                AssignmentId = quiz.AssignmentId,
-                Assignments = quiz,
-                StudentId = student.Id,
-                studentCourseConnection = studentCourseConnection,
-                Grade = null,
-                SubmissionDate = DateTime.Now,
-                TextSubmission = "Test Submission"
+                AssignId = quiz.AssignId,
+                StudentId = student.UserId,
+                Points = null,
+                Submitted = DateTime.Now,
+                Submission = "Test Submission"
             };
 
             // Save them ALL to the database
             _context.User.Add(instructor);
+            _context.UserInfo.Add(instructorInfo);
             _context.User.Add(student);
+            _context.UserInfo.Add(studentInfo);
             _context.Courses.Add(course);
-            _context.InstructorCourseConnection.Add(instructorCourseConnection);
-            _context.StudentCourseConnection.Add(studentCourseConnection);
+            _context.InstructorConnection.Add(instructorCourseConnection);
+            _context.StudentConnection.Add(studentCourseConnection);
             _context.Assignments.Add(quiz);
-            _context.StudentAssignment.Add(studentAssignment);
+            _context.StudentAssignment.Add(studentQuiz);
             _context.SaveChanges();
 
             // Create the controller
-            _controller = new CourseController(_notificationService, _context, _mockAuth.Object);
+            _controller = new CourseController( _context, _mockAuth.Object);
 
             // Grade the quiz with a random number between 0 and the max score
             Random random = new Random();
-            string grade = random.Next(0, quiz.AssignmentPoints).ToString();
-            _controller.UpdateSubmission(quiz.AssignmentId, student.Id, grade);
+            string grade = random.Next(0, quiz.MaxPoints).ToString();
+            _controller.UpdateSubmission(quiz.AssignId, student.UserId, grade);
 
             // Check if the student's grade is equal to the grade given
-            StudentAssignments quizFromDb = _context.StudentAssignment.Find(studentAssignment.AssignmentGradeId);
-            Assert.AreEqual(grade, quizFromDb.Grade.ToString());
+            StudentAssignments quizFromDb = _context.StudentAssignment
+                .FirstOrDefault(q => q.AssignId == quiz.AssignId && q.StudentId == student.UserId);
+            Assert.AreEqual(grade, quizFromDb.Points.ToString());
 
             // Cleanup EVERYTHING (Moq, InMemoryDatabase, controller, everything)
             _context.Database.EnsureDeleted();
@@ -224,15 +223,6 @@ namespace Spencer_Unit_Tests
         [TestMethod]
         public void InstructorGradeAssignment()
         {
-            // Setup Moq and InMemoryDatabase
-            // Create an instructor and student
-            // Create a course
-            // Connect the instructor and student to the course
-            // Create a assignment
-            // Save them ALL to the database
-            // Grade the assignment with a random number between 0 and the max score
-            // Check if the student's grade is equal to the grade given
-
             _mockAuth = new Mock<IAuthentication>();
             var options = new DbContextOptionsBuilder<UserContext>()
                 .UseInMemoryDatabase(databaseName: "TestDatabase")
@@ -244,125 +234,124 @@ namespace Spencer_Unit_Tests
             // Create the instructor
             User instructor = new User
             {
-                Id = 1,
+                UserId = 1,
                 Email = "instructor@weber.edu",
-                Password = "Test",
+                Password = "Test"
+            };
+            UserInfo instructorInfo = new UserInfo
+            {
+                UserId = 1,
                 FirstName = "Test",
                 LastName = "Instructor",
-                BirthDate = new System.DateTime(1990, 1, 1),
-                UserRole = "Instructor"
+                Role = "Instructor",
+                PFP = "Test",
+                BirthDate = new DateOnly(1990, 1, 1)
             };
 
             // Create the student
             User student = new User
             {
-                Id = 2,
+                UserId = 2,
                 Email = "student@weber.edu",
-                Password = "Test",
+                Password = "Test"
+            };
+            UserInfo studentInfo = new UserInfo
+            {
+                UserId = 2,
                 FirstName = "Test",
                 LastName = "Student",
-                BirthDate = new System.DateTime(1990, 1, 1),
-                UserRole = "Student"
+                Role = "Student",
+                PFP = "Test",
+                BirthDate = new DateOnly(1990, 1, 1)
             };
 
             // Create the department (for the course)
             Departments department = new Departments
             {
-                DepartmentId = 1,
-                DepartmentName = "Test Department"
+                DeptId = 1,
+                DeptName = "Test Department"
             };
 
             // Create the course
             Courses course = new Courses
             {
                 CourseId = 1,
-                DepartmentId = department.DepartmentId,
-                Department = department,
-                CourseNumber = "TS1010",
+                DeptId = department.DeptId,
+                CourseNum = 1010,
                 CourseName = "Test Course",
-                CourseDescription = "Test Description",
-                NumberOfCredits = 3,
+                CourseDesc = "Test Description",
+                Credits = 3,
                 Capacity = 30,
                 Campus = "Ogden",
                 Building = "Science",
-                RoomNumber = 101,
-                DaysOfTheWeek = "MWF",
+                Room = 101,
+                Days = "MWF",
                 StartTime = new TimeOnly(9, 0),
-                EndTime = new TimeOnly(10, 0),
+                StopTime = new TimeOnly(10, 0),
                 Semester = "Fall",
                 Year = 2024,
-                Instructors = new List<User> { instructor }
+                Color = "Test",
+                Picture = "Test"
             };
 
             // Connect the instructor and student to the course
-            InstructorCourseConnection instructorCourseConnection = new InstructorCourseConnection
+            InstructorConnection instructorCourseConnection = new InstructorConnection
             {
-                InstructorCourseConnectionId = 1,
-                InstructorID = instructor.Id,
-                Instructor = instructor,
-                CourseId = course.CourseId,
-                Course = course
+                InstructorId = instructor.UserId,
+                CourseId = course.CourseId
             };
-            StudentCourseConnection studentCourseConnection = new StudentCourseConnection
+            StudentConnection studentCourseConnection = new StudentConnection
             {
-                StudentCourseId = 1,
-                StudentID = student.Id,
-                User = student,
-                CourseId = course.CourseId,
-                Course = course
+                StudentId = student.UserId,
+                CourseId = course.CourseId
             };
-            course.instructorCourseConnections = new List<InstructorCourseConnection> { instructorCourseConnection };
-            course.StudentCourseConnection = new List<StudentCourseConnection> { studentCourseConnection };
 
             // Create the assignment
             Assignment assignment = new Assignment
             {
-                AssignmentId = 1,
-                InstructorCourseId = instructorCourseConnection.InstructorCourseConnectionId,
-                InstructorCourse = instructorCourseConnection,
-                AssignmentDescription = "Test assignment",
-                AssignmentName = "assignment",
-                DueDateAndTime = new System.DateTime(2021, 12, 31),
-                AssignmentPoints = 100,
-                AssignmentType = "Text",
+                AssignId = 1,
                 CourseId = course.CourseId,
-                Course = course
+                AssignName = "assignment",
+                AssignDesc = "Test assignment",
+                AssignType = "Text",
+                MaxPoints = 100,
+                DueDate = DateTime.Now,
             };
 
             // Connect the student to the assignment
             StudentAssignments studentAssignment = new StudentAssignments
             {
-                AssignmentGradeId = 1,
-                AssignmentId = assignment.AssignmentId,
-                Assignments = assignment,
-                StudentId = student.Id,
-                studentCourseConnection = studentCourseConnection,
-                Grade = null,
-                SubmissionDate = DateTime.Now,
-                TextSubmission = "Test Submission"
+                AssignId = assignment.AssignId,
+                StudentId = student.UserId,
+                Points = null,
+                Submitted = DateTime.Now,
+                Submission = "Test Submission"
             };
 
             // Save them ALL to the database
             _context.User.Add(instructor);
+            _context.UserInfo.Add(instructorInfo);
             _context.User.Add(student);
+            _context.UserInfo.Add(studentInfo);
             _context.Courses.Add(course);
-            _context.InstructorCourseConnection.Add(instructorCourseConnection);
-            _context.StudentCourseConnection.Add(studentCourseConnection);
+            _context.InstructorConnection.Add(instructorCourseConnection);
+            _context.StudentConnection.Add(studentCourseConnection);
             _context.Assignments.Add(assignment);
             _context.StudentAssignment.Add(studentAssignment);
             _context.SaveChanges();
 
             // Create the controller
-            _controller = new CourseController(_notificationService, _context, _mockAuth.Object);
+            _controller = new CourseController(_context, _mockAuth.Object);
 
-            // Grade the assignment with a random number between 0 and the max score
+            // Grade the quiz with a random number between 0 and the max score
             Random random = new Random();
-            string grade = random.Next(0, assignment.AssignmentPoints).ToString();
-            _controller.UpdateSubmission(assignment.AssignmentId, student.Id, grade);
+            string grade = random.Next(0, assignment.MaxPoints).ToString();
+            _controller.UpdateSubmission(assignment.AssignId, student.UserId, grade);
 
             // Check if the student's grade is equal to the grade given
-            StudentAssignments assignmentFromDb = _context.StudentAssignment.Find(studentAssignment.AssignmentGradeId);
-            Assert.AreEqual(grade, assignmentFromDb.Grade.ToString());
+            StudentAssignments quizFromDb = _context.StudentAssignment
+                .FirstOrDefault(q => q.AssignId == assignment.AssignId && q.StudentId == student.UserId);
+            Assert.AreEqual(grade, quizFromDb.Points.ToString());
 
             // Cleanup EVERYTHING (Moq, InMemoryDatabase, controller, everything)
             _context.Database.EnsureDeleted();
